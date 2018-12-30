8d30a27c3617131509e22f605f0369307647e95e
hhiroki <h-hiroki@users.noreply.github.com>
2018-12-30 21:53:52 +0900

    ⛳️Factoryを追加

---
+++ database/factories/EloquentCustomerFactory.php
@@ -0,0 +1,11 @@
+<?php
+declare(srtict_types=1);
+
+use App\Eloquent\EloquentCustomer;
+use Faker\Generator as Faker;
+
+$factory->define(EloquentCustomer::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+    ];
+});
